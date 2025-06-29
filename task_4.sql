bb1f0c7a8342feada0a43bcb6bb69313b984ab1c
Ibk-okiki <ibukunoluwaadelokiki@gmail.com>
2025-06-29 23:42:57 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -2,4 +2,6 @@
 -- Script to print full description of Books table from alx_book_store database
 -- Using INFORMATION_SCHEMA to get table structure information
 
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Books';
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA 
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
